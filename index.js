15700e3d0d0ca7a05bf2224c9f7d56c6977c1492
theLemon505 <76224503+theLemon505@users.noreply.github.com>
2022-02-03 17:29:13 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function donate(){
+    document.location.href = "/client/pages/donate/donate.html"
+}   
+
+function home(){
+    document.location.href = "/client/index.html"
+}
+
+function contact(){
+    document.location.href = "/client/pages/contact/contact.html"
+}
+
+function services(){
+    document.location.href = "/client/pages/services/services.html"
+}
+
+function signup(){
+
+}
